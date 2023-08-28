6e485409ec01fc41611a10d6db2d185e48010dd6
khanhpn <phamkhanh90@gmail.com>
2023-08-28 22:44:59 +0700

    api login

--- app/controllers/api/v1/authorization/authorization.rb
+++ app/controllers/api/v1/authorization/authorization.rb
@@ -2,43 +2,40 @@ module Api
   module V1
     module Authorization
       class Authorization < Grape::API
-        get :test do
-          status 200
-          {data: 1}
-        end
-
-        desc 'Register a user'
-        params do
-          requires :email, type: String, desc: 'Type email address'
-          requires :password, type: String, desc: 'Type password'
-          requires :password_confirmation, type: String, desc: 'Type password confirmation'
-        end
-        post :register do
-          user = User.new(email: params[:email])
-          user.password = params[:password]
-          if user.save
-            status 200
-            user
-          else
-            status 400
-            { error: 'Invalid email address or password' }
+        namespace :authorization do
+          desc 'Register a user'
+          params do
+            requires :email, type: String, desc: 'Type email address'
+            requires :password, type: String, desc: 'Type password'
+            requires :password_confirmation, type: String, desc: 'Type password confirmation'
           end
-        end
+          post :register do
+            user = User.new(email: params[:email])
+            user.password = params[:password]
+            if user.save
+              status 200
+              user
+            else
+              status 400
+              { error: 'Invalid email address or password' }
+            end
+          end
 
-        desc 'Login with email and password'
-        params do
-          requires :email, type: String, desc: 'Type email address'
-          requires :password, type: String, desc: 'Type password'
-        end
-        post :login do
-          user = User.find_by(email: params[:email])
-          if user.authenticate(params[:password])
-            jwt = Auth.encode({user: user.id})
-            status 200
-            { jwt: jwt }
-          else
-            status 401
-            { error: 'Invalid email or password' }
+          desc 'Login with email and password'
+          params do
+            requires :email, type: String, desc: 'Type email address'
+            requires :password, type: String, desc: 'Type password'
+          end
+          post :login do
+            user = User.find_by(email: params[:email])
+            if user.authenticate(params[:password])
+              jwt = Auth.encode({user: user.id})
+              status 200
+              { jwt: jwt }
+            else
+              status 401
+              { error: 'Invalid email or password' }
+            end
           end
         end
       end
